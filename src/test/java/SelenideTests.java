d24ad6d3857594db2d3b7d3bfa4279b284433d8d
nazartal007 <a.nazarov@robo.finance>
2021-06-02 10:08:37 +0700

    Added tests and other

---
+++ src/test/java/SelenideTests.java
@@ -0,0 +1,28 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selectors;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideTests {
+
+    @Test
+    public void softAssertPageOnGitHub(){
+        open("https://github.com/selenide/selenide");
+        $("[data-content='Wiki']").click();
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $x("//*[@class='d-block' and text()='SoftAssertions']").click();
+        $x("//*[text()='Using JUnit5 extend test class:']/../following-sibling::div ").should(visible);
+    }
+
+    @Test
+    public void dragAndDropTest(){
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").should(text("A"));
+        $("#column-a").dragAndDropTo($("#column-b"));
+        $("#column-a").should(text("B"));
+    }
+
+}
